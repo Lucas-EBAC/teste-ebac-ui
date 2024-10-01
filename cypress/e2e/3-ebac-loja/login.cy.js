65efd29e7ea9a3f7f94386728ab829c8321e1d27
unknown <lucastsunoda3@gmail.com>
2024-10-01 19:43:02 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-ebac-loja/login.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+    
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('lucas.teste@teste.com')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+        
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, lucas.teste (não é lucas.teste? Sair)') 
+
+     })
+
+})
