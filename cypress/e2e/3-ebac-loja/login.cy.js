def3b8edf5909fc6b06285cb017440ca01a55b18
unknown <lucastsunoda3@gmail.com>
2024-10-02 18:33:53 -0300

    Aula EBAC 121

--- cypress/e2e/3-ebac-loja/login.cy.js
+++ cypress/e2e/3-ebac-loja/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -14,7 +15,6 @@ describe('Funcionalidade: Login', () => {
         cy.get('#username').type('lucas.teste@teste.com')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, lucas.teste (não é lucas.teste? Sair)') 
 
      })
@@ -23,7 +23,6 @@ describe('Funcionalidade: Login', () => {
         cy.get('#username').type('lucas@teste.com')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido.')
      });
 
@@ -31,8 +30,25 @@ describe('Funcionalidade: Login', () => {
         cy.get('#username').type('lucas.teste@teste.com')
         cy.get('#password').type('teste')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-error').should("exist")
+     });
+
+     it('Deve fazer login com sucesso usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, lucas.teste (não é lucas.teste? Sair)')
+     });
+
+     it('Dever fazer login com sucesso usando fixture', () => {
+        cy.fixture('perfil').then( dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha , { log: false })
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, lucas.teste (não é lucas.teste? Sair)')
+        
+        })
+
      });
 
 })
